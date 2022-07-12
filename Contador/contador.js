839629ad5dbd841107924a28940acad0fa7963da
MuKenzo <101224959+MuKenzo@users.noreply.github.com>
2022-07-11 22:37:19 -0300

    Add files via upload

--- Contador/contador.js
+++ Contador/contador.js
@@ -1,20 +1,8 @@
+document.getElementById('adicionar').addEventListener('click', increment, true);
+document.getElementById('subtrair').addEventListener('click', decrement, true);
+
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
-
-/* Contador (0 a 10)
-function increment() {
-    if(currentNumber < 10 && currentNumber >= 0) {
-        currentNumber = currentNumber + 1;
-        currentNumberWrapper.innerHTML = currentNumber;
-    }
-}
-
-function decrement() {
-    if(currentNumber < 10 && currentNumber >= 1) {
-        currentNumber = currentNumber - 1;
-        currentNumberWrapper.innerHTML = currentNumber;
-    }
-} */
 
 function increment() {
     currentNumber = currentNumber + 1;
